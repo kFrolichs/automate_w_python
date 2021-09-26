9ccf487cc03087cfbed96a4b8d981960c81b2ac6
Koen Frolichs <k.frolichs@gmail.com>
2021-09-26 16:24:58 +0200

    Start and failure of PDF extraction

---
+++ pdf_table_extractor/pdf_table_extractor.py
@@ -0,0 +1,12 @@
+import camelot as cm
+# #
+# # # Open the pdf with Camelot
+# input_pdf = cm.read_pdf('india_factsheet_economic_n_hdi.pdf')
+input_pdf = cm.read_pdf('us-030.pdf')
+
+
+# import ctypes
+#
+# from ctypes.util import find_library
+#
+# find_library("".join(("gsdll", str(ctypes.sizeof(ctypes.c_voidp) * 8), ".dll")))
